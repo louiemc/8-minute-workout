28651559280b4417f318fcd75c70fbe304ba0f7c
louiemc <clouie2@gmail.com>
2021-08-09 20:59:40 -0400

    more ids and hidden class added. variables added using ids and times declard for timers

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// DOM MANIPULATED VARIABLES
+const introMessage = document.getElementById("intro-message");
+const startBtn = document.getElementById("start-btn");
+const againBtn = document.getElementById("again-btn");
+const startMessage = document.getElementById("start-message");
+// Countdown times for 10 seconds, 45 seconds and 15 seconds (not sure if i need all of them tbd)
+const tenSecondTimer = document.getElementById("ten-second-timer");
+const fifteenSecondTimer = document.getElementById("fifteen-second-timer");
+const fortyfiveSecondTimer = document.getElementById("fortfive-second-timer");
+
+// Timer countdowns
+const tenSeconds = 10;
+const fifteenSeconds = 15;
+const fortyfiveSeconds = 45;
